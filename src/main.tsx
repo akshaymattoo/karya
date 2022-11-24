4df66c344694d32c1730573c61e2acc79b9cf710
akshaymattoo <akshaymattoo@gmail.com>
2022-11-24 15:37:06 -0800

    removed imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import ErrorPage from './error-page';
 import './index.css';
 import { ChakraProvider } from '@chakra-ui/react';
-import ScratchPad from './components/ScratchPad';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>

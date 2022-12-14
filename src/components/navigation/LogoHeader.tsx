f980ad5bbe5f03e46f4fb6ff050e6457fe2891d4
akshaymattoo <akshaymattoo@gmail.com>
2022-12-13 18:53:30 -0800

    padding

--- src/components/navigation/LogoHeader.tsx
+++ src/components/navigation/LogoHeader.tsx
@@ -4,7 +4,7 @@ import logo from '/assets/logo.png';
 
 export default function LogoHeader() {
   return (
-    <Box bg="blue.200">
+    <Box p="2" bg="blue.200">
       <Image boxSize={'60px'} src={logo} alt="logo" />
     </Box>
   );

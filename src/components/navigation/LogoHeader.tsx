efa8fad49b15f3130f90eae7a4e371c09a4eb127
akshaymattoo <akshaymattoo@gmail.com>
2022-12-13 19:09:57 -0800

    fixed header

--- src/components/navigation/LogoHeader.tsx
+++ src/components/navigation/LogoHeader.tsx
@@ -4,7 +4,7 @@ import logo from '/assets/logo.png';
 
 export default function LogoHeader() {
   return (
-    <Box p="2" bg="blue.200">
+    <Box p="5" bg="blue.200" position={'fixed'}>
       <Image boxSize={'60px'} src={logo} alt="logo" />
     </Box>
   );

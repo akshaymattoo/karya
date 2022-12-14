a308304957f9dea7b5e5d94f43a6d1bda57cd996
akshaymattoo <akshaymattoo@gmail.com>
2022-12-13 22:04:55 -0800

    added padding

--- src/components/navigation/LogoHeader.tsx
+++ src/components/navigation/LogoHeader.tsx
@@ -4,7 +4,7 @@ import logo from '/assets/logo.png';
 
 export default function LogoHeader() {
   return (
-    <Box p="1" bg="blue.200" position={'fixed'} w="100%">
+    <Box p={[3, 3, 1, 1]} bg="blue.200" position={'fixed'} w="100%">
       <Image boxSize={'60px'} src={logo} alt="logo" />
     </Box>
   );

98066a4d008872495864f2219dd8a6978eb05929
akshaymattoo <akshaymattoo@gmail.com>
2023-01-07 16:04:49 -0800

    updated logo at home screen

--- src/components/navigation/LogoHeader.tsx
+++ src/components/navigation/LogoHeader.tsx
@@ -1,10 +1,11 @@
 import { Box, Image } from '@chakra-ui/react';
 import logo from '/assets/logo.png';
+import logoColor from '/assets/logo-color.png';
 
 export default function LogoHeader() {
   return (
-    <Box pt={[8, 3, 1, 1]} bg="blue.200" position={'fixed'} w="100%">
-      <Image boxSize={'60px'} src={logo} alt="logo" />
+    <Box pt={[8, 3, 1, 1]} ml={10} position={'fixed'} w="100%">
+      <Image width={'50px'} objectFit="cover" src={logoColor} />
     </Box>
   );
 }
